e9953019b0026ecef7e80ac1547da3d0c6bd0eb4
Leonardo Lobato <leonardo.lobato@gmail.com>
2016-03-09 02:44:07 -0300

    added plumber, less compilation, autoprefixer, del and watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,32 @@
 var gulp = require('gulp'),
     args = require('yargs').argv,
     config = require('./gulp.config')(),
+    del = require('del'),
     $ = require('gulp-load-plugins')({ lazy: true });
+
+gulp.task('watcher:less', function() {
+    gulp.watch(config.less, ['styles']);
+});
+
+gulp.task('styles', ['clean:styles'], function() {
+    log('Compiling Less --> CSS');
+
+    return gulp.src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.autoprefixer({
+            browser: [
+                'last 2 version',
+                '> 5%'
+            ]
+        }))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean:styles', function() {
+    var files = config.temp + '**/*.css';
+    clean(files);
+});
 
 gulp.task('vet', function() {
     log('Analyzing source with JSHing and JSCS');
@@ -15,6 +40,11 @@ gulp.task('vet', function() {
         .pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
         .pipe($.jshint.reporter('fail'));
 });
+
+function clean(path) {
+    log($.util.colors.red.bold('Cleaning: ' + path));
+    del.sync(path);
+}
 
 function log(msg) {
     if (typeof(msg) === 'object') {
